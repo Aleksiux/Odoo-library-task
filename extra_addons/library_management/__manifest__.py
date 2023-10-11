558c45f2ac5b42242e7dd471dc348e847155e77f
Aleksas Kaminskas <aleksiuxx@gmail.com>
2023-10-11 10:12:55 +0300

    added initial commit with library module

---
+++ extra_addons/library_management/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Library Management',
+    'version': '13.0.1.0.0',
+    'category': 'Library',
+    'author': 'Aleksas',
+    'website': '',
+    'depends': ['base'],
+    'data': [
+        'views/library_document_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': False,
+}
